c4a3a77c2191d00ec0c600f429deba62a07afd6c
rayhankinan <rayhankinan@gmail.com>
2023-11-06 20:08:55 +0700

    Add interactive roads

---
+++ interactive-roads/mapbox/style.js
@@ -0,0 +1,44 @@
+
+var styleJSON = {
+    "version": 8,
+    "name": "qgis2web export",
+    "pitch": 0,
+    "light": {
+        "intensity": 0.2
+    },
+    "sources": {
+        "OSMStandard_0": {
+            "type": "raster",
+            "tiles": ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+            "tileSize": 256
+        },
+        "roads_1": {
+            "type": "geojson",
+            "data": json_roads_1
+        }
+                    },
+    "sprite": "",
+    "glyphs": "https://glfonts.lukasmartinelli.ch/fonts/{fontstack}/{range}.pbf",
+    "layers": [
+        {
+            "id": "background",
+            "type": "background",
+            "layout": {},
+            "paint": {
+                "background-color": "#ffffff"
+            }
+        },
+        {
+            "id": "lyr_OSMStandard_0_0",
+            "type": "raster",
+            "source": "OSMStandard_0"
+        },
+        {
+            "id": "lyr_roads_1_0",
+            "type": "line",
+            "source": "roads_1",
+            "layout": {},
+            "paint": {'line-width': 0.9285714285714285, 'line-opacity': 1.0, 'line-color': '#e31a1c'}
+        }
+],
+}
