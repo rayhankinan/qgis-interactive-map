e239e47be1b018641bb9e96ad9bd4fdd616256f7
rayhankinan <rayhankinan@gmail.com>
2023-11-06 20:46:16 +0700

    Add interactive cities

---
+++ interactive-cities/mapbox/style.js
@@ -0,0 +1,44 @@
+
+var styleJSON = {
+    "version": 8,
+    "name": "qgis2web export",
+    "pitch": 0,
+    "light": {
+        "intensity": 0.2
+    },
+    "sources": {
+        "OSMStandard_0": {
+            "type": "raster",
+            "tiles": ["http://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+            "tileSize": 256
+        },
+        "cities_1": {
+            "type": "geojson",
+            "data": json_cities_1
+        }
+                    },
+    "sprite": "",
+    "glyphs": "https://glfonts.lukasmartinelli.ch/fonts/{fontstack}/{range}.pbf",
+    "layers": [
+        {
+            "id": "background",
+            "type": "background",
+            "layout": {},
+            "paint": {
+                "background-color": "#ffffff"
+            }
+        },
+        {
+            "id": "lyr_OSMStandard_0_0",
+            "type": "raster",
+            "source": "OSMStandard_0"
+        },
+        {
+            "id": "lyr_cities_1_0",
+            "type": "circle",
+            "source": "cities_1",
+            "layout": {},
+            "paint": {'circle-radius': ['/', 7.142857142857142, 2], 'circle-color': '#e31a1c', 'circle-opacity': 1.0, 'circle-stroke-width': 1, 'circle-stroke-color': '#232323'}
+        }
+],
+}
